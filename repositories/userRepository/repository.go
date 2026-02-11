75bc924defcf48d0545c3a893142ecfb318669f3
Sharif <ah.sharif.biz@gmail.com>
2026-02-11 23:30:43 +0330

    fix(login): decouple UserRepository from sql.Row package.

--- repositories/userRepository/repository.go
+++ repositories/userRepository/repository.go
@@ -5,17 +5,21 @@ import (
 	"database/sql"
 )
 
+type Query interface {
+	QueryRow(query string, args ...interface{}) *sql.Row
+}
+
 type UserRepository struct {
-	db *sql.DB
+	db Query
 }
 
-func New(db *sql.DB) *UserRepository {
-	return &UserRepository{db: db}
+func New(query Query) *UserRepository {
+	return &UserRepository{db: query}
 }
 
-func (r *UserRepository) GetUserByPhone(phone string) (entities.User, error) {
+func (r *UserRepository) GetUserByPhone(phone string) (*entities.User, error) {
 	row := r.db.QueryRow(
-		`SELECT id, uuid, phone, first_name, last_name, created_at, updated_at
+		`SELECT id, uuid, phone, first_name, last_name, password,created_at, updated_at
 		 FROM users
 		 WHERE phone = $1 AND deleted_at IS NULL`,
 		phone,
@@ -28,15 +32,13 @@ func (r *UserRepository) GetUserByPhone(phone string) (entities.User, error) {
 		&user.Phone,
 		&user.FirstName,
 		&user.LastName,
+		&user.Password,
 		&user.CreatedAt,
 		&user.UpdatedAt,
 	)
 	if err != nil {
-		if err == sql.ErrNoRows {
-			return entities.User{}, nil
-		}
-		return entities.User{}, err
+		return nil, err
 	}
 
-	return user, nil
+	return &user, nil
 }
